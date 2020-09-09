40811c01eca03edede87a02af3172ea2ec178f17
Kyle Hagler <khagler.kh@gmail.com>
2020-09-08 21:44:29 -0500

    View changed to SafeAreaView

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+//SafeAreaView makes sure content doesn't get covered up by the top 'notch'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      
+    </SafeAreaView>
   );
 }
 
